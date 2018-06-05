0ff1cfd75268f8fe14eea9847e95129a60c33b60
subinyoon <sbyoon92@gmail.com>
2018-06-05 19:03:03 +0900

    subin commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     menuLink();
     landingPage();
     aboutusPage();
+    portfolioDetailPage();
+    portfolioPage();
         
 }); //document ready END
 
@@ -20,6 +22,15 @@ function aboutusPage(){
     aboutusFilter();
 }
 
+function portfolioDetailPage(){
+    content1();
+    content2();
+}
+
+function portfolioPage(){
+    toDetail();
+}
+
 /*menu link---------------------------------------------*/
 function menuLink(){
     $(".menu ul li").on('click', function(){
@@ -33,7 +44,7 @@ function menuLink(){
             var url = window.location.href;
             console.log(url);
         
-            if( url == "http://127.0.0.1:58233/rb_index.html" || url == "http://127.0.0.1:58233/rb_index.html#goto_contact" ){
+            if( url == "http://127.0.0.1:60946/rb_index.html" || url == "http://127.0.0.1:60946/rb_index.html#goto_contact" ){
                 $('html,body').stop().animate({
                 scrollTop: $(".wrap_3").offset().top}, 600, 'swing');
             }else{
@@ -280,4 +291,85 @@ function aboutusFilter(){
 
 /*-------------------------------------------------------*/
 
+/*portfolio : linking to portfolio detail*/
+function toDetail(){
+    $(".portfolio_wrap .column .contents, .client_boxes .boxes").on('click', function(){
+        window.location.href='rb_portfolio_detail.html';
+    });
+} // 이부분은 상세페이지 더 나오면 디테일하게 잡을 것!!!!!
+
+/*portfolio detail : content 1 onclick effect*/
+function content1(){
+    
+    $(".content_1_wrap .box").on('click', function(){
+        
+        //get box1 fixed url
+        var fixdiv = $(".content_1_wrap .box_1").attr('id');
+            //console.log(div);
+        var getfixUrl = getComputedStyle($('#'+fixdiv)[0]).getPropertyValue("background-image");
+            //console.log(getfixUrl);
+        var slicefixUrl = getfixUrl.slice(27,43);
+            //console.log(slicefixUrl);
+        var finalfixUrl = ".."+slicefixUrl;
+            //console.log(finalfixUrl);
+
+        
+        //get $(this).onclicked url
+        var i = $(this).index()+1;
+            //console.log(i);
+        var div = $(this).attr('id');
+            //console.log(div);
+        var getUrl = getComputedStyle($('#'+div)[0]).getPropertyValue("background-image");
+            //console.log(getUrl);
+        var sliceUrl = getUrl.slice(27,43);
+            //console.log(sliceUrl);
+        var finalUrl = ".."+sliceUrl;
+            //console.log(finalUrl);
+        
+        //action
+        $(".content_1_wrap .box_1").css({'background-image':'url('+finalUrl+')'});
+        $(".content_1_wrap .box_"+i).css({'background-image':'url('+finalfixUrl+')'});
+    });
+}
+
+/*portfolio detail : content 2 onclick effect*/
+function content2(){
+    $(".content_2_wrap .box").on('click', function(){
+
+        var i = $(this).index()+4;
+            //console.log(i);
+        
+        if(i == 4){
+            $(".box_"+i).css({
+            'opacity':'1',
+            'box-shadow':'2px 2px 8px #e2e2e2'
+            });
+            
+            $(".box_5, .box_6").css({
+            'opacity':'0.3',
+            'box-shadow':'none'
+            });  
+        }else if(i == 5){
+            $(".box_"+i).css({
+            'opacity':'1',
+            'box-shadow':'2px 2px 8px #e2e2e2'
+            });
+            
+            $(".box_4, .box_6").css({
+            'opacity':'0.3',
+            'box-shadow':'none'
+            });     
+        }else if(i == 6){
+            $(".box_"+i).css({
+            'opacity':'1',
+            'box-shadow':'2px 2px 8px #e2e2e2'
+            });
+            
+            $(".box_5, .box_4").css({
+            'opacity':'0.3',
+            'box-shadow':'none'
+            });     
+        }  
+    });
+}
       
