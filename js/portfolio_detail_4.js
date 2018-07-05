73c23faf789304d0a52826a4cf70616e8f8e1fbd
subinyoon <sbyoon92@gmail.com>
2018-07-05 12:10:52 +0900

    subin commit

--- js/portfolio_detail_4.js
+++ js/portfolio_detail_4.js
@@ -1,8 +1,6 @@
 (function($) { 
 
 $(document).ready(function(){
-    alert("load");
-    console.log("loaded")
     portfolioDetailPage();
 }); //document ready END
 
@@ -13,29 +11,28 @@ function portfolioDetailPage(){
 function showIllustration(){
     
     $(".inner_wrap .content_wrap .content_1_wrap .slides").on('click', function(){
-        alert("clicked");
         //get slide1 fixed url
         var fixdiv = $(".content_1_wrap .slide_1").attr('id');
 
         var getfixUrl = getComputedStyle($('#'+fixdiv)[0]).getPropertyValue("background-image");
-            console.log(getfixUrl);
+            //console.log(getfixUrl);
         var slicefixUrl = getfixUrl.slice(27,40);
-            console.log(slicefixUrl);
+            //console.log(slicefixUrl);
         var finalfixUrl = "../"+slicefixUrl;
-            console.log(finalfixUrl);
+            //console.log(finalfixUrl);
 
         
         //get $(this).onclicked url
         var i = $(this).index()+1;
-            console.log(i);
+            //console.log(i);
         var div = $(this).attr('id');
-            console.log(div);
+            //console.log(div);
         var getUrl = getComputedStyle($('#'+div)[0]).getPropertyValue("background-image");
-            console.log(getUrl);
+            //console.log(getUrl);
         var sliceUrl = getUrl.slice(27,40);
-            console.log(sliceUrl);
+            //console.log(sliceUrl);
         var finalUrl = "../"+sliceUrl;
-            console.log(finalUrl);
+            //console.log(finalUrl);
         
         //action
         $(".content_1_wrap .slide_1").css({'background-image':'url('+finalUrl+')'});
