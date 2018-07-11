cf549220f98cdfb26f1476dbc0c9c7465dc84d79
subinyoon <sbyoon92@gmail.com>
2018-07-11 23:09:42 +0900

    subin commit

--- js/m_portfolio_detail_bino.js
+++ js/m_portfolio_detail_bino.js
@@ -15,28 +15,28 @@ function showIllustration(){
         var fixdiv = $(".content_1_wrap .slide_1").attr('id');
 
         var getfixUrl = getComputedStyle($('#'+fixdiv)[0]).getPropertyValue("background-image");
-            //console.log(getfixUrl);
-        var slicefixUrl = getfixUrl.slice(27,40);
-            //console.log(slicefixUrl);
-        var finalfixUrl = "../"+slicefixUrl;
-            //console.log(finalfixUrl);
+            console.log(getfixUrl);
+        var slicefixUrl = getfixUrl.slice(27,37);
+            console.log(slicefixUrl);
+        var finalfixUrl = slicefixUrl;
+            console.log(finalfixUrl);
 
         
         //get $(this).onclicked url
         var i = $(this).index()+1;
             //console.log(i);
         var div = $(this).attr('id');
-            //console.log(div);
+            console.log(div);
         var getUrl = getComputedStyle($('#'+div)[0]).getPropertyValue("background-image");
-            //console.log(getUrl);
-        var sliceUrl = getUrl.slice(27,40);
-            //console.log(sliceUrl);
-        var finalUrl = "../"+sliceUrl;
-            //console.log(finalUrl);
+            console.log(getUrl);
+        var sliceUrl = getUrl.slice(27,37);
+            console.log(sliceUrl);
+        var finalUrl = sliceUrl;
+            console.log(finalUrl);
         
         //action
-        $(".content_1_wrap .slide_1").css({'background-image':'url('+finalUrl+')'});
-        $(".content_1_wrap .slide_"+i).css({'background-image':'url('+finalfixUrl+')'}); 
+        $(".content_1_wrap .slide_1").css({'background-image':'url(img'+finalUrl+')'});
+        $(".content_1_wrap .slide_"+i).css({'background-image':'url(img'+finalfixUrl+')'}); 
     });
 }
 
