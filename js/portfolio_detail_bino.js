3c3cb01f23dee08113662a717c42317367661e2c
subinyoon <sbyoon92@gmail.com>
2018-07-11 22:52:58 +0900

    subin commit

--- js/portfolio_detail_bino.js
+++ js/portfolio_detail_bino.js
@@ -15,24 +15,24 @@ function showIllustration(){
         var fixdiv = $(".content_1_wrap .slide_1").attr('id');
 
         var getfixUrl = getComputedStyle($('#'+fixdiv)[0]).getPropertyValue("background-image");
-            //console.log(getfixUrl);
+            console.log(getfixUrl);
         var slicefixUrl = getfixUrl.slice(27,40);
-            //console.log(slicefixUrl);
+            console.log(slicefixUrl);
         var finalfixUrl = "../"+slicefixUrl;
-            //console.log(finalfixUrl);
+            console.log(finalfixUrl);
 
         
         //get $(this).onclicked url
         var i = $(this).index()+1;
-            //console.log(i);
+            console.log(i);
         var div = $(this).attr('id');
-            //console.log(div);
+            console.log(div);
         var getUrl = getComputedStyle($('#'+div)[0]).getPropertyValue("background-image");
-            //console.log(getUrl);
+            console.log(getUrl);
         var sliceUrl = getUrl.slice(27,40);
-            //console.log(sliceUrl);
+            console.log(sliceUrl);
         var finalUrl = "../"+sliceUrl;
-            //console.log(finalUrl);
+            console.log(finalUrl);
         
         //action
         $(".content_1_wrap .slide_1").css({'background-image':'url('+finalUrl+')'});
