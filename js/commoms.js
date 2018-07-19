b1fc31dd3f6fb721a3d433a3026f3331fa104d5c
subinyoon <sbyoon92@gmail.com>
2018-07-19 14:43:20 +0900

    subin commit spinner

--- js/commoms.js
+++ js/commoms.js
@@ -1,8 +1,7 @@
 window.onload = function () {
     $("#submit_btn").click(function () {
 
-        //spinner & blanket ON
-        $(".wrap_3 .spinner").css({"display":"block"});
+        //spinner ON
         $(".wrap_3 .blanket").css({"display":"block"});
 
         const name = $("#studentName").val(),
@@ -22,6 +21,7 @@ window.onload = function () {
             alert("연락 가능한 이메일을 작성해주세요");
             return;
         }
+        
         $.ajax({
             type: "POST",
             url: "/emailto",
@@ -34,16 +34,14 @@ window.onload = function () {
             crossDomain : true,
             success: function (data) {
                 
-                //spinner & blanket OFF
-                $(".wrap_3 .spinner").css({"display":"none"});
+                //spinner OFF
                 $(".wrap_3 .blanket").css({"display":"none"});
                 
                 alert("문의가 성공적으로 접수되었습니다.");
             },
             error: function (error) {
                 
-                //spinner & blanket OFF
-                $(".wrap_3 .spinner").css({"display":"none"});
+                //spinner OFF
                 $(".wrap_3 .blanket").css({"display":"none"});
 
                 alert('관리자에게 문의해 주세요')
