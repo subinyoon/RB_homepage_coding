65a6514d7142b66560d4201cbadb1a8828fd6320
subinyoon <sbyoon92@gmail.com>
2018-07-20 17:38:58 +0900

    subin commit

--- js/pc_ptd_bino.js
+++ js/pc_ptd_bino.js
@@ -2,16 +2,7 @@
 
 $(document).ready(function(){
     portfolioDetailPage();
-    
-    if (!("console" in window) || !("firebug" in console))
- {
-     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
-     "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
 
-     window.console = {};
-     for (var i = 0; i < names.length; ++i)
-         window.console[names[i]] = function() {}
- }
     
 }); //document ready END
 
