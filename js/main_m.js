41121d59f086e2dad9f19740755d9eb6cb4713f2
subinyoon <sbyoon92@gmail.com>
2018-07-25 11:52:05 +0900

    subin commit

--- js/main_m.js
+++ js/main_m.js
@@ -379,8 +379,30 @@ function snsLink(){
 
 /*detect mobile devices*/
 function mobileDevice{
-    var x = "User-agent header sent: " + navigator.userAgent;
-    alert(x);
+    
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    }; 
+    
+    if( isMobile.iOS() ) alert('iOS');
+    if( isMobile.any() ) alert('Mobile');
 }
 
 
